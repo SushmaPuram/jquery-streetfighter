9ebadc6091084d3ec1dd776ae20a871b3b22f8a4
Sushma Puram <sushmapuram13@gmail.com>
2015-05-17 14:09:03 -0700

    Js modified

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
       $('.ryu-still').hide();
       $('.ryu-ready').show();
   })
+})
 
    .mouseleave(function() {
     $('.ryu-ready').hide();
@@ -25,16 +26,31 @@ $(document).ready(function() {
     function() {
       $(this).hide();
       $(this).css('left', '520px');
-    });
+      });
+
+    /*keydown(function (e) {
+     if (e.which == 88) {
+         alert('You pressed X');
+     } */
+   /* Pressing X
+   .keydown(function( event e) { 
+    if ( e.which == 88 ) {
+   e.preventDefault();
+    alert( "Handler for keydown called." );  
+   })
+    /*$('.ryu').on("keydown", { keyCode: 64} 
+     {     //(function(event e) {
+    alert( "Handler for keydown called." );
+       })*/
   })
   .mouseup(function() {
     // ryu goes back to his ready position
      $('.ryu-throwing').hide();
-  $('.ryu-ready').show();
-  });
-});
+     $('.ryu-ready').show();
+  })
 function playHadouken () {
   $('#hadouken-sound')[0].volume = 0.5;
   $('#hadouken-sound')[0].load();
   $('#hadouken-sound')[0].play();
 }
+
