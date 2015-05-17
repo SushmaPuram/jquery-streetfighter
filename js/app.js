f36f0b4f9c8498d1d537d366cd275a84ee2cb635
Sushma Puram <sushmapuram13@gmail.com>
2015-05-17 14:33:49 -0700

    Js modified

--- js/app.js
+++ js/app.js
@@ -1,56 +1,50 @@
 $(document).ready(function() {
 
-	/* Hovering */
-	$('.ryu').mouseenter(function() {
-     /*To show ready and hide still position */
-      $('.ryu-still').hide();
-      $('.ryu-ready').show();
+  /* Hovering */
+  $('.ryu').mouseenter(function() {
+    /*To show ready and hide still position */
+    $('.ryu-still').hide();
+    $('.ryu-ready').show();
   })
-})
-
-   .mouseleave(function() {
+  .mouseleave(function() {
     $('.ryu-ready').hide();
     $('.ryu-still').show();
   })
-   .mousedown(function() {
+
+  .mousedown(function() {
     // play hadouken sound
     playHadouken();
     $('.ryu-ready').hide();
     $('.ryu-throwing').show();
-    /*$('.hadouken').show();*/
 
     // show hadouken and animate it to the right of the screen
     $('.hadouken').finish().show().animate(
-    {'left': '1020px'},
-    500,
-    function() {
-      $(this).hide();
-      $(this).css('left', '520px');
-      });
-
-    /*keydown(function (e) {
-     if (e.which == 88) {
-         alert('You pressed X');
-     } */
-   /* Pressing X
-   .keydown(function( event e) { 
-    if ( e.which == 88 ) {
-   e.preventDefault();
-    alert( "Handler for keydown called." );  
-   })
-    /*$('.ryu').on("keydown", { keyCode: 64} 
-     {     //(function(event e) {
-    alert( "Handler for keydown called." );
-       })*/
+        {'left': '1020px'},
+        500,
+        function() {
+          $(this).hide();
+          $(this).css('left', '520px');
+        });
   })
   .mouseup(function() {
     // ryu goes back to his ready position
-     $('.ryu-throwing').hide();
-     $('.ryu-ready').show();
-  })
-function playHadouken () {
-  $('#hadouken-sound')[0].volume = 0.5;
-  $('#hadouken-sound')[0].load();
-  $('#hadouken-sound')[0].play();
-}
+    $('.ryu-throwing').hide();
+    $('.ryu-ready').show();
+  });
 
+  $(document).keydown(function (e) {
+    if (e.keyCode == 88) {
+      console.log('You pressed X');
+    }
+  })
+  /*.keyup(function(e) {
+
+  });*/
+
+  function playHadouken () {
+    $('#hadouken-sound')[0].volume = 0.5;
+    $('#hadouken-sound')[0].load();
+    $('#hadouken-sound')[0].play();
+  }
+
+});
